c899270c81ff46f24171066e13de76cb1e7612d9
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2024-12-12 19:47:00 -0300

    uptade

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    effect: 'fade',
+    speed: 500
+});
